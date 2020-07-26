99877a744c7f5a76bd1d27856bc9beeebc8e6924
well <well@corp.globo.com>
2020-07-26 12:35:27 -0300

    Create subnetworks

--- terraform/teste01/network.tf
+++ terraform/teste01/network.tf
@@ -2,3 +2,10 @@ resource "google_compute_network" "teste01" {
     name                    = "teste01"
     auto_create_subnetworks = false
 }
+
+resource "google_compute_subnetwork" "teste01" {
+    name            = "teste01"
+    ip_cidr_range   = "10.10.1.0/24" 
+    region          = "us-central1"
+    network         = "teste01"
+}

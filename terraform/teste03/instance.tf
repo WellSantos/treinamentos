650fe0763545c4edcb86c8effc7378b5ff2c9329
well <well@corp.globo.com>
2020-07-27 09:08:42 -0300

    Explict dependencies

--- terraform/teste03/instance.tf
+++ terraform/teste03/instance.tf
@@ -1,4 +1,5 @@
 resource "google_compute_instance" "teste03" {
+    depends_on   = [google_compute_network.teste03]
     name         = "teste03"
     machine_type = "f1-micro"
     zone         = "us-central1-a"
@@ -10,6 +11,6 @@ resource "google_compute_instance" "teste03" {
     }
 
     network_interface {
-        network = google_compute_network.teste03.self_link
+        network = "teste03"
     }
 }

ec5927c646b95032f26e4684ef093a0e8b24afc6
well <well@corp.globo.com>
2020-07-28 12:56:33 -0300

    Modules I

---
+++ terraform/teste06/network/gcp-network/variables.tf
@@ -0,0 +1,13 @@
+variable "name" {
+    type        = string
+    description = "Deverá ser adicionado um descrição para sua rede"
+}
+variable "description" {
+    type        = string
+    description = "Coloque um nome que aparecerá no dashboard para sua rede"
+}
+variable "auto_create_subnetworks" {
+    type        = bool
+    description = "Será criado automaticamente redes para todas regiões? default: true"
+    default     = true
+}
---
+++ terraform/teste06/network/network.tf
@@ -0,0 +1,6 @@
+module "network" {
+    source  = "./gcp-network"
+
+    name    = "teste06"
+    description = "Rede do teste06"   
+}
